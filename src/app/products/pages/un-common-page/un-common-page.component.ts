734730aea2d3dd942a0b93bb8e19e0217ebf9f7f
Facundo <wehrlifacundo9@gmail.com>
2023-07-29 13:40:33 -0300

    feat: uncommon pipes (i18nSelect) & (i18Plural)

--- src/app/products/pages/un-common-page/un-common-page.component.ts
+++ src/app/products/pages/un-common-page/un-common-page.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-un-common-page',
   templateUrl: './un-common-page.component.html',
-  styleUrls: ['./un-common-page.component.css']
+  styleUrls: ['./un-common-page.component.css'],
 })
 export class UnCommonPageComponent {
+  public name: string = 'Facundo';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+  //i18nPlural
+  public clients: string[] = [
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Facundo',
+    'Natalia',
+  ];
+
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=3': 'tenemos 2 clientes esperando.',
+    other: ' tenemos # clientes esperando',
+  };
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
 }
