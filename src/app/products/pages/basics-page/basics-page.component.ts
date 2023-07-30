af6841a27fe3d632ab958302cbbf329ab67c092b
Facundo <wehrlifacundo9@gmail.com>
2023-07-30 14:14:58 -0300

    del: eliminat unused css files

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrls: ['./basics-page.component.css']
 })
 export class BasicsPageComponent {
   public nameLower: string = 'facundo';
