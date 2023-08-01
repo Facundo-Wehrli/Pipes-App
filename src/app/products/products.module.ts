813c11238070cf234d6e851b7ec2f13d0fe59180
Facundo <wehrlifacundo9@gmail.com>
2023-08-01 09:46:02 -0300

    feat: custom Pipes

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -7,6 +7,9 @@ import { NumbersPageComponent } from './pages/numbers-page/numbers-page.componen
 import { UnCommonPageComponent } from './pages/un-common-page/un-common-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 
 @NgModule({
@@ -14,7 +17,11 @@ import { OrderComponent } from './pages/order/order.component';
     BasicsPageComponent,
     NumbersPageComponent,
     UnCommonPageComponent,
-    OrderComponent
+    OrderComponent,
+    // Pipes
+    ToggleCasePipe,
+    CanFlyPipe,
+    SortByPipe
   ],
   imports: [
     CommonModule,
